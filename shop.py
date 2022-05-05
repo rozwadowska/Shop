041330ad87aafb55dcf3f7694df554e46bf80697
rozwadowska <101137855+rozwadowska@users.noreply.github.com>
2022-05-05 16:09:44 +0200

    Update shop.py

--- shop.py
+++ shop.py
@@ -52,6 +52,69 @@ print ("The price is: ", TB1_price + TB2_price + TB3_price, "zl/net")
 
 
 
+____________________________________________________________
 
+Products = [
+    ["Black_Tshirt (number 1)", 105, 75],
+    ["White_Tshirt (number 2)", 105, 85],
+    ["Gray_Tshirt (number 3)", 105, 80]
+    ]
+
+for productName, price, quantity in Products:
+    print("Product name:", productName)
+    print("Price:", price)
+    print("Quantity:", quantity)
+    print("\n")
+
+HowMany = 0
+HowMany2 = 0
+HowMany3 = 0
+
+decision = True
+while decision == True:
+    choice = input("T-Shirt number: ")
+    if (choice == "1"):
+        print("The price is:", Products[0][1], "PLN/net", "In stock: ", Products[0][2])
+        if (choice == "1"):
+            HowMany = int(input("How many pieces?: "))
+            print("The price is: ", Products[0][1] * HowMany)
+            print("And then in stock: ", Products[0][2] - HowMany)
+    if (choice == "2"):
+        print("The price is:", Products[1][1], "PLN/net", "In stock: ", Products[1][2])
+        if (choice == "2"):
+            HowMany2 = int(input("How many pieces?: "))
+            print("The price is: ", Products[1][1] * HowMany2)
+            print("And then in stock: ", Products[1][2] - HowMany2)
+    if (choice == "3"):
+        print("The price is:", Products[2][1], "PLN/net", "In stock: ", Products[2][2])
+        if (choice == "3"):
+            HowMany3 = int(input("How many pieces?: "))
+            print("The price is: ", Products[2][1] * HowMany3)
+            print("And then in stock: ", Products[2][2] - HowMany3)
+
+    decision = input("Do you want to continue (yes/no): ")
+    if decision == "yes":
+        decision = True
+    elif decision == "no":
+        decision = False
+        break
+    else:
+        print('Wrong Comment!')
+        decision = input("Do you want to continue (Yes/No): ")
+
+print("Great job!")
+
+
+TB1_price = Products[0][1] * HowMany
+TB2_price = Products[1][1] * HowMany2
+TB3_price = Products[2][1] * HowMany3
+
+VAT = 23
+VAT2 = (1 + VAT / 100)
+full_price = TB1_price + TB2_price + TB3_price
+full_price_VAT = full_price * VAT2
+
+
+print ("The price is: ", full_price_VAT, "PLN/gross")
 
 
